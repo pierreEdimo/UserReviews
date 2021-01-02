25a062bed53aa3b72f811f810fee8fc2410c560d
pierre edimo <pierredimo@live.com>
2021-01-02 15:40:57 +0100

    i added the widget to update a comment

--- lib/screens/CommentScreen.dart
+++ lib/screens/CommentScreen.dart
@@ -36,6 +36,148 @@ class _CommentScreenState extends State<CommentScreen> {
   _CommentScreenState({@required this.reviewId});
   @override
   Widget build(BuildContext context) {
+    void _updateCommentSheet(String content, int id) async {
+      TextEditingController _updateController = TextEditingController()
+        ..text = content;
+      showModalBottomSheet(
+        context: context,
+        builder: (context) {
+          return Container(
+            color: Color(0xFF737373),
+            child: Container(
+              padding: EdgeInsets.all(20.0),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(10.0),
+                  topRight: Radius.circular(10.0),
+                ),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      maxLines: 13,
+                      controller: _updateController,
+                      decoration: InputDecoration(
+                        contentPadding:
+                            EdgeInsets.only(left: 10.0, top: 20.0, right: 10.0),
+                        border: OutlineInputBorder(borderSide: BorderSide()),
+                        hintText: "updateComment",
+                        hintStyle:
+                            TextStyle(fontSize: 16.0, color: Colors.black),
+                      ),
+                    ),
+                  ),
+                  InkWell(
+                    onTap: () async {
+                      _commentService
+                          .updateComment(_updateController.text, id)
+                          .then((_) => _loadComments())
+                          .then((_) => Navigator.of(context).pop());
+                    },
+                    child: Container(
+                      margin: EdgeInsets.only(top: 20.0),
+                      padding: EdgeInsets.all(20.0),
+                      decoration: BoxDecoration(
+                          color: Colors.deepPurple,
+                          borderRadius: BorderRadius.circular(10.0)),
+                      child: Center(
+                        child: Text(
+                          "send",
+                          style: TextStyle(
+                              fontSize: 18.0,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white),
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          );
+        },
+      );
+    }
+
+    void _showCommentModal(
+        String authorId, int id, String content, context) async {
+      String userId = await storage.read(key: "userId");
+      showModalBottomSheet(
+        context: context,
+        builder: (context) {
+          if (authorId == userId) {
+            return Container(
+              color: Color(0xFF737373),
+              height: 130,
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(20.0),
+                      topRight: Radius.circular(20.0)),
+                ),
+                child: Column(
+                  children: [
+                    ListTile(
+                      onTap: () async => _commentService
+                          .deleteComment(id)
+                          .then((_) => _loadComments())
+                          .then((_) => Navigator.of(context).pop()),
+                      leading: Icon(
+                        Icons.delete_outlined,
+                        color: Colors.black,
+                      ),
+                      title: Text(
+                        'Delete Comment',
+                        style: TextStyle(color: Colors.black),
+                      ),
+                    ),
+                    ListTile(
+                      onTap: () async {
+                        Navigator.of(context).pop();
+                        _updateCommentSheet(content, id);
+                      },
+                      leading: Icon(
+                        Icons.edit,
+                        color: Colors.black,
+                      ),
+                      title: Text(
+                        'Edit Comment',
+                        style: TextStyle(color: Colors.black),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            );
+          } else {
+            return Container(
+              color: Color(0xFF737373),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(20.0),
+                    topRight: Radius.circular(20.0),
+                  ),
+                ),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.flag_outlined,
+                    color: Colors.black,
+                  ),
+                  title: Text('Report'),
+                ),
+              ),
+            );
+          }
+        },
+      );
+    }
+
     void _showCommentSheet() async {
       showModalBottomSheet(
           context: context,
@@ -165,9 +307,6 @@ class _CommentScreenState extends State<CommentScreen> {
                                   child: Text(
                                     "there are no Comments yet , please be the first !",
                                     textAlign: TextAlign.center,
-                                    style: TextStyle(
-                                        fontWeight: FontWeight.bold,
-                                        fontSize: 18),
                                   ),
                                 ),
                               )
@@ -201,10 +340,15 @@ class _CommentScreenState extends State<CommentScreen> {
                                                       fontSize: 19),
                                                 ),
                                                 IconButton(
-                                                    icon: Icon(Icons
-                                                        .more_horiz_outlined),
-                                                    onPressed: () =>
-                                                        print("Hello More !"))
+                                                  icon: Icon(Icons
+                                                      .more_horiz_outlined),
+                                                  onPressed: () =>
+                                                      _showCommentModal(
+                                                          comment.authorId,
+                                                          comment.id,
+                                                          comment.body,
+                                                          context),
+                                                )
                                               ],
                                             ),
                                             Container(
@@ -212,8 +356,6 @@ class _CommentScreenState extends State<CommentScreen> {
                                                   top: 20.0, bottom: 20.0),
                                               child: Text(
                                                 comment.body,
-                                                style:
-                                                    TextStyle(fontSize: 16.0),
                                               ),
                                             )
                                           ],
