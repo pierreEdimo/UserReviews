04f923d4b82a4e0676b67a11e7f892737cc03b21
pierre edimo <pierredimo@live.com>
2021-01-02 15:39:43 +0100

    i added the authorId

--- lib/model/Comment.dart
+++ lib/model/Comment.dart
@@ -4,19 +4,16 @@ class Comment {
   final int id;
   final int reviewId;
   final String body;
+  final String authorId;
   final RegisterModel author;
 
-  Comment({
-    this.id,
-    this.reviewId,
-    this.body,
-    this.author,
-  });
+  Comment({this.id, this.reviewId, this.body, this.author, this.authorId});
 
   factory Comment.fromJson(Map<String, dynamic> json) {
     return Comment(
         id: json['id'] as int,
         reviewId: json['reviewId'] as int,
+        authorId: json['authorId'] as String,
         author: RegisterModel.fromJson(json['author']),
         body: json['body'] as String);
   }
