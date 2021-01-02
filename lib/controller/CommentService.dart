3618ca4e8776867f98faf903117b75eee6d8c6ab
pierre edimo <pierredimo@live.com>
2021-01-02 15:38:26 +0100

    i added the method to delete and update comment

--- lib/controller/CommentService.dart
+++ lib/controller/CommentService.dart
@@ -3,6 +3,8 @@ import 'dart:convert';
 import 'package:http/http.dart';
 import 'package:userCritiqs/model/Comment.dart';
 
+import '../main.dart';
+
 class CommentService {
   Future<Response> addComment(String body, String authorId, int reviewId) {
     return post(
@@ -18,6 +20,33 @@ class CommentService {
         },
       ),
     );
+  }
+
+  Future<Response> deleteComment(int id) async {
+    String jwt = await storage.read(key: "jwt");
+
+    Response response = await delete(
+        'https://uservoice20201218092231.azurewebsites.net/api/Comments/$id',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer ' + jwt
+        });
+
+    return response;
+  }
+
+  Future<Response> updateComment(String body, int id) async {
+    String jwt = await storage.read(key: "jwt");
+
+    Response response = await put(
+        'https://uservoice20201218092231.azurewebsites.net/api/Comments/$id',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer ' + jwt
+        },
+        body: jsonEncode({'id': id, 'body': body}));
+
+    return response;
   }
 
   Future<List<Comment>> getComments(int reviewId) async {
